b8b9025cc3f99b6425ad81dfd9fd2d8c942fbeb9
Xinshuo Weng <xinshuo.weng@gmail.com>
2016-10-20 20:41:36 -0400

    delete unnecessary files

--- 150207_CarPictures/150207_CarPictures/opencv.h
+++
@@ -1,9 +0,0 @@
-
-
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc_c.h>
-#include <opencv2/gpu/gpu.hpp>
-
-using namespace cv;
